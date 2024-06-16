1cfc4e82810bbd6f2c3230c45ee4e73f160b27a8
Anonymous2906 <130924965+Anonymous2906@users.noreply.github.com>
2024-06-16 17:16:13 +0200

    updates

--- repo/service.autoexec/autoexec.py
+++ repo/service.autoexec/autoexec.py
@@ -1,7 +1,43 @@
 import xbmc
 import xbmcvfs
+import zipfile
+import os
+import urllib.request
+
+def download_zip(url, download_path):
+    try:
+        urllib.request.urlretrieve(url, download_path)
+        return True
+    except Exception as e:
+        xbmc.log(f"Failed to download file: {e}", xbmc.LOGERROR)
+        return False
+
+def extract_zip(zip_path, extract_path):
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+        return True
+    except Exception as e:
+        xbmc.log(f"Failed to extract file: {e}", xbmc.LOGERROR)
+        return False
 
 def main():
+    # Télécharger et extraire le fichier ZIP
+    zip_url = "http://tobal.duckdns.org:805/api/public/dl/lRM-2m2W/config_vstream/resources.zip"
+    download_path = 'special://home/userdata/addon_data/plugin.video.vstream/resources.zip'
+    extract_path = 'special://home/addons/plugin.video.vstream/'
+
+    if download_zip(zip_url, download_path):
+        if extract_zip(download_path, extract_path):
+            xbmc.log("File downloaded and extracted successfully", xbmc.LOGINFO)
+        else:
+            xbmc.log("Failed to extract the ZIP file", xbmc.LOGERROR)
+    else:
+        xbmc.log("Failed to download the ZIP file", xbmc.LOGERROR)
+
+    # Supprimer le fichier ZIP après extraction
+    xbmcvfs.delete(download_path)
+
     # Supprimer les fichiers de cache vidéo et pastebin
     xbmcvfs.delete('special://home/userdata/addon_data/plugin.video.vstream/video_cache.db')
     xbmcvfs.delete('special://home/userdata/addon_data/plugin.video.vstream/pastebin_cache.db')
@@ -17,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
